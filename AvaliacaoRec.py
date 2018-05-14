39d8933c3ddde2c6d1b71e76df194cde79857eef
Alex <alexhandel96@googlemail.com>
2018-05-14 18:38:55 -0300

    message

--- AvaliacaoRec.py
+++ AvaliacaoRec.py
@@ -5,6 +5,7 @@ import numpy as np
 
 K1=1
 b=0.75
+beta=1
 
 DOCS=['O peã e o caval são pec de xadrez. O caval é o melhor do jog.',
 'A jog envolv a torr, o peã e o rei.',
@@ -213,7 +214,36 @@ def calculatePrecision(relevant, recovered):
 	l=float(len(inter))
 	A=float(len(recovered))
 	result = l/A     
-	return result   
+	return result  
+
+def fMeasure(j, ranking):
+	result = 0
+	Rj=calculateRecall(R1, ranking)
+	Pj=calculatePrecision(R1, ranking)
+	numerator=(1+(beta*beta))*Pj*Rj
+	denominator=((beta*beta)*Pj)+Rj
+	result=numerator/denominator
+ 	return result
+
+def interpolatedPrecision(recall, precision, recovered):
+	levels=[]
+	counter=0
+	for i in range(len(recovered)):
+		if recovered[i]+1 in R1:
+			counter+=1
+			rankslice=recovered[:i+1]
+			r=calculateRecall(R1, rankslice)
+			p=calculatePrecision(R1, rankslice)
+			levels.append([r,p])
+	levels.append([recall, precision])
+	print "\nPRECISAO INTERPOLADA PARA OS 11 NIVEIS:"
+	for i in np.arange(0.0, 1.1, 0.1):
+		maxvalue=0
+		for j in levels:
+			if j[0]>=i and j[1]> maxvalue:
+				maxvalue=j[1]
+		print i, maxvalue
+	pass
 
 def VectorSpaceModelImplementation(docList, separators, stopwords, query):
 	print docList,separators,stopwords,query
@@ -241,9 +271,25 @@ def VectorSpaceModelImplementation(docList, separators, stopwords, query):
 
 	recall=calculateRecall(R1, ranking)    
 	precision=calculatePrecision(R1, ranking)
+	F1 = fMeasure(len(ranking), ranking)
 	print "\nRECALL: ", recall
 	print "\nPRECISION: ", precision
+	print "\nMEDIDA F1: ", F1, "\n\n"   
 
+	#calculo de revocação e precisão a cada relevenate recuperado
+	tRanking=ranking[::-1] 
+	counter=0
+	for i in range(len(ranking)):
+		if tRanking[i]+1 in R1:
+			counter+=1
+			rankslice=tRanking[:i+1]
+			r=calculateRecall(R1, rankslice)
+			p=calculatePrecision(R1, rankslice)
+			print "REVOCAÇÃO E PRECISÃO NO RELEVANTE NUMERO ", counter
+			print "REVOCAÇÃO ", r
+			print "PRECISÃO: ", p
+	#precisão interpolada
+	interpolatedPrecision(recall, precision, tRanking)
 	pass
 
 def simbm25(document, incidence, wordList, query, avgDocLength, niList):
@@ -268,7 +314,6 @@ def calculateRankingsProbabilistic(documents,query,docIncidence, wordList):
 	for doc in documents:
 		docLengthList.append(len(doc))
 	avgDocLen=np.mean(docLengthList)
-	print "AVERAGE DOC LENGTH: ", avgDocLen    
 
 	documentFrequencies = []
 	for word in wordList:
@@ -276,7 +321,6 @@ def calculateRankingsProbabilistic(documents,query,docIncidence, wordList):
 		for doc in documents:
 			if word in doc: count+=1
 		documentFrequencies.append(count)
-	print "DOCUMENT FREQUENCIES: ", documentFrequencies
 
 
 	count=0
@@ -302,7 +346,6 @@ def probabilisticModelImplementation(docList, separators, stopwords, query):
 
 	rankings = calculateRankingsProbabilistic(finalDocList,finalQuery,docIncidenceMatrix,wordList)  
 	similarities=dict(enumerate(rankings))  
-	print similarities
 	temp=dict(similarities)
 	for i in similarities:
 		if similarities[i]<0:   
@@ -318,8 +361,26 @@ def probabilisticModelImplementation(docList, separators, stopwords, query):
 		c+=1
 	recall=calculateRecall(R1, ranking)    
 	precision=calculatePrecision(R1, ranking)
+	F1 = fMeasure(len(ranking), ranking)
 	print "\nRECALL: ", recall
 	print "\nPRECISION: ", precision
+	print "\nMEDIDA F1: ", F1, "\n\n"  
+
+	#calculo de revocação e precisão a cada relevenate recuperado
+	tRanking=ranking[::-1] 
+	counter=0
+	for i in range(len(ranking)):
+		if (tRanking[i]+1) in R1:
+			counter+=1
+			rankslice=tRanking[:i+1]
+			r=calculateRecall(R1, rankslice)
+			p=calculatePrecision(R1, rankslice)
+			print "REVOCAÇÃO E PRECISÃO NO RELEVANTE NUMERO ", counter
+			print "REVOCAÇÃO ", r
+			print "PRECISÃO: ", p
+
+	#precisão interpolada
+	interpolatedPrecision(recall, precision, tRanking)
 	pass
 
 #PROGRAMA COMEÇA EXECUÇÃO AQUI
